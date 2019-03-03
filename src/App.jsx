5ec14dd3e0408fd04c6284d498d4c9108818f64c
Abhishek Soni <abhisheksoni2720@gmail.com>
2019-03-03 20:37:04 +0530

    completed basic preact template app

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import { h, Component } from "preact";
+
+export default () => {
+  return <h1>Hello, there!</h1>;
+};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { h, render } from "preact";
+
+import App from "./App";
+
+// render an instance of App into the root div:
+render(<App />, document.getElementById("root"));
